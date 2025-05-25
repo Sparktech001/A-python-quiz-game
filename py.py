c1e54c85cec19d26a2928ae67246abe9b8b1f9bd
Sparktech001 <daniajoseph111@gmail.com>
2025-05-26 00:02:37 +0100

    Add files via upload
    Games

--- py.py
+++ py.py
@@ -1,30 +1,41 @@
 import random
-#A rock paper scissor game using python radom module, if statments and while loop
-running = True
-options = ("rock", "paper", "scissors")
-while running:
-    player = input("Enter an options (Rock, Paper, Scissors): ").lower()
-    computer = random.choice(options)
 
-    while player not in options:
-        player = input("Not among the options (Rock, Paper, Scissors): ").lower()
-    print(f"The computer chose: {computer}")
-    print(f"The player chose: {player}")
-    if player == computer:
-        print("It's a tie")
+# A random guessing game using imported random module from python
 
-    elif player == "sisccors" and computer == "paper":
-        print("You won!!!")
+lowest_num_guess = random.randint(5, 40)
+highest_num_guess = (random.randint(1, 50))
 
-    elif player == "rock" and computer == "scissors":
-        print("You won!!!")
+# testing the randomness by changing the values
 
-    elif player == "paper" and computer == "rock":
-        print("You won!!!")
+lowest_num = lowest_num_guess
+highest_num = highest_num_guess
 
+# I created random numbers for the values of the lowest and highest numbers
+
+answer = random.randint(lowest_num, highest_num)
+guesses = 0
+print("---------PYTHON NUMBER GUESSING GAME-------")
+print(f"Select a number between {lowest_num} and {highest_num}")
+while True:
+    guess = input("Enter your guess(q to quit): ")
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+        if guess < lowest_num or guess > highest_num:
+            print("That number is out of range")
+            print(f"Pls select a number between {lowest_num} and {highest_num}")
+        elif guess < answer:
+            print("Too low, try again")
+        elif guess > answer:
+            print("Too high, try again")
+        elif guess == answer:
+            print(f"CORRECT!! The answer was {answer}.")
+            print(f"You tried for {guesses} times.")
+            break
+    elif guess == "q":
+        break
     else:
-        print("You lost")
-
-    paly_again = input("Play again (y/n)").lower()
-    if paly_again != "y":
-        running = False
+        print("INVALID GUESS")
+        print(f"Select a number between {lowest_num} and {highest_num}")
+"""This random number game runs by using the imported random module. it uses the while loop to loop those some conditions,
+    it then prints the total number of trials after a successful game, it uses the break statment to escape the loop"""
